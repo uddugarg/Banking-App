fb9695a100ad19358fccf0fbe1f86e439d7c725c
uddugarg <udivyy.99@gmail.com>
2024-06-25 18:07:29 +0530

    chore: Update Home component with HeaderBox and RightSidebar
    The Home component is updated to include the HeaderBox component for displaying a welcome message and the RightSidebar component for showing the user's profile details and a list of their banks.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,41 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Udit",
+    lastName: "Garg",
+    email: "udivyy.99@gmail.com",
+  };
 
-    const loggedIn = { firstName: 'Udit' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and manage your account and translations efficiently"
+          />
 
-    return (
-        <section className='home'>
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox
-                        type="greeting"
-                        title="Welcome"
-                        user={loggedIn.firstName || "Guest"}
-                        subtext="Access and manage your account and translations efficiently"
-                    />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
+    </section>
+  );
+};
 
-                    <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
-                </header>
-            </div>
-        </section>
-    )
-}
-
-export default Home
+export default Home;
