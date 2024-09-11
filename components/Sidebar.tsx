22c3109b5c631eab516d2c985c34697003ce653b
uddugarg <udivyy.99@gmail.com>
2024-09-11 23:08:19 +0530

    chore: Add PlaidLink component to Sidebar for user navigation
    This commit adds the PlaidLink component to the Sidebar, allowing users to navigate to the Plaid integration for banking functionality. The PlaidLink component is now rendered as a navigation item in the Sidebar's navigation menu.
    
    This change enhances the user experience by providing a direct and convenient way for users to access the Plaid integration.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "@/node_modules/next/image";
 import Link from "@/node_modules/next/link";
 import { usePathname } from "@/node_modules/next/navigation";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -50,7 +51,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           );
         })}
-        USER
+        <PlaidLink user={user} />
       </nav>
 
       <Footer user={user} />
